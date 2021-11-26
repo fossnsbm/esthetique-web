2c6cc83c8c77540dbbcea09f8cf278729d042905
Seniya Dewmina <seniyadewmina25@gmail.com>
2021-11-26 22:26:03 +0530

    Updated
    Register Section

---
+++ Register Section/App.js
@@ -0,0 +1,13 @@
+import React from "react"; 
+import Register from "./Register";
+
+function App(){
+  
+  return(
+    <div className="App">
+      <Register/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Register Section/Register.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './App.css';
+
+function Register(){
+    return (
+        <div className="Register">
+            <h1>Ready for the challenge?</h1>
+            <a href="https://www.youtube.com">
+            <button className="click">Register now</button></a>
+        </div>
+    )
+}
+export default Register;
